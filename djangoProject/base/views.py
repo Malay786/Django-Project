55ed1a9468c8e8d6352f425dd14cf22843bbad46
Malay <malaydewari@gmail.com>
2025-09-15 23:16:21 +0530

    mobile responsiveness

--- djangoProject/base/views.py
+++ djangoProject/base/views.py
@@ -75,7 +75,7 @@ def home(request):
         Q(description__icontains=q) |
         Q(host__username__icontains=q)
         )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]   #only give first five topics
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -200,3 +200,16 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)     # use return before redirect everytime
 
     return render(request, 'base/update-user.html', {'form':form})
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q) #filter() with no arguments returns everything (same as .all())
+    
+    context={'topics': topics}
+    return render(request, 'base/topics.html', context)
+
+
+def activityPage(request):
+    room_messages= Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
